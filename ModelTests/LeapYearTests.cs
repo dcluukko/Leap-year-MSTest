13ac8a53197d4ef8e62b0d54829fa7f064e03605
Dana <dcluukko@gmail.com>
2020-03-01 21:39:45 -0800

    created a c# test file directory

---
+++ ModelTests/LeapYearTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.Tests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    // Test method will go here
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+
+    [TestMethod]
+
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+    }
+
+    [TestMethod]
+
+    public void IsLeapYear_MuliplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    } 
+
+    [TestMethod]
+
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
+  }
+}
